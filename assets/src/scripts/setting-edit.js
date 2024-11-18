9e2de2faeb3e4f79216165ea0c9576ec8a890ad9
Mikhail-2547 <artiprice@gmail.com>
2024-11-18 14:55:38 +0200

    auto commit

--- assets/src/scripts/setting-edit.js
+++ assets/src/scripts/setting-edit.js
@@ -3,15 +3,43 @@ const selectors = {
     emptyState: '.empty-state',
 }
 
-
-
-
-
-
-const url = new URL('http://127.0.0.1:5500/WEB-Shopify--MIT-21-Mikhail-Melik-Kazarian/index.html')
-
 const initialState = document.querySelector(selectors.stateJson).textContent;
 
 const state = JSON.parse(initialState);
-const params = new URLSearchParams(state[1]);
+const params = new URLSearchParams(state);
 
+const url = window.location.href;
+
+
+
+// function getUrlParams(url) {
+//     const params = {};
+//     const parser = new URL(url);
+//     console.log(parser);
+//     const queryString = parser.search.slice(1);
+//     const queryArray = queryString.split("&");
+    
+//     queryArray.forEach(param => {
+//       const [key, value] = param.split('=');
+//       params[decodeURIComponent(key)] = decodeURIComponent(value || '');
+//     });
+  
+//     console.log(params);
+    
+//     return params;
+//   }
+function getUrlParams(url) {
+    const urlInit = new URL(url);
+    const params = new URLSearchParams(urlInit.search);
+    params.append('id','6');
+
+}
+
+
+getUrlParams(url);
+
+
+
+// function URL_ident(url) {
+//     param
+// }
